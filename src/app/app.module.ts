b32ad7f80c3bd1bbada599f0748de37661b82d31
Tobiasz Nogaj <tobnog@hotmail.com>
2022-05-21 12:40:05 +0200

    Add login, menu and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginButtonsComponent } from './login-buttons/login-buttons.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginButtonsComponent,
+    MainMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
